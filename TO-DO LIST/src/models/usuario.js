0479dc9c099b58d35dc359f5ac4d84fc26f9bfe8
Daniel Tellez Cordero <d.tellezcordero@ugto.mx>
2021-06-10 19:51:22 -0500

    Página de login

--- TO-DO LIST/src/models/usuario.js
+++ TO-DO LIST/src/models/usuario.js
@@ -3,10 +3,11 @@ const Schema = mongoose.Schema;
 
 const TaskSchema = new Schema({
     nombre: String,
+    correo: String,
     password: String,
     password_confirm: String,
     date: {type: Date, dafault: Date.now},
 });
 
-module.exports=mongoose.model('usuario',TaskSchema);
+module.exports=mongoose.model('user',TaskSchema);
 
